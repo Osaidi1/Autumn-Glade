9f6d345fd2e3554c93f2303199fb6b6e53dd3fc2
Osaidi1 <hassanosaid123@gmail.com>
2025-09-19 21:05:15 +0200

    Fixed anims, completly new script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,109 +1,80 @@
 extends CharacterBody2D
 
-const walk_speed = 50
-const run_speed = 200
+const walkspeed = 45
+const runspeed = 150
+const JUMP_VELOCITY = -280.0
+const friction = 100
 var SPEED = 300.0
-var JUMP_VELOCITY = -280.0
-var is_crouching = false
-var is_sliding = false
-var is_healing = false
-var is_climbing = false
-var is_attacking = false
-var is_dying = false
+var stopping
+var is_moving = false
 var is_jumping = false
+var is_crouching = false
 @onready var animator: AnimatedSprite2D = $AnimatedSprite2D
-@onready var collision: CollisionShape2D = $Collision
-var idlecshape = preload("res://collisions/idle.tres")
-var crouchslidecshape = preload("res://collisions/croutchslide.tres")
+@onready var crouch_on_wait: Timer = $AnimatedSprite2D/crouch_on_wait
+@onready var crouch_off_wait: Timer = $AnimatedSprite2D/crouch_off_wait
 
-func _ready() -> void:
-	collision.shape = idlecshape
-
-func change_speed():
+func walkorrun(SPEED):
 	if Input.is_action_pressed("run"):
-		SPEED = run_speed
+		SPEED = runspeed
+		
 	else:
-		SPEED = walk_speed
+		SPEED = walkspeed
+		
+	return SPEED
 
-func change_direction(direction):
+func jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		is_jumping = true
+		velocity.y = JUMP_VELOCITY 
+
+func facing(direction):
 	if direction < 0:
 		animator.flip_h = true
-	elif direction > 0:
+	if direction > 0:
 		animator.flip_h = false
-	
-func handle_crouch():
-	if Input.is_action_pressed("crouch"):
-		is_crouching = true
-		collision.shape = crouchslidecshape
-		collision.position.y = 4.5
-	elif Input.is_action_just_released("crouch"):
-		is_crouching = false
-		collision.shape = idlecshape
-		collision.position.y = 0
-	
-func handle_jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		animator.play("jump")
-	
-func play_animations(direction):
-	if is_on_floor():
-		if is_crouching:
-			animator.play("crouch on")
-			animator.play("crouch")
-			if Input.is_action_just_released("crouch"):
-				animator.play("crouch off")
-		else:
-			if direction == 0:
-				animator.play("idle")
-				
-			else:
-				if Input.is_action_pressed("run"):
-					animator.play("run")
-				else:
-					animator.play("walk")
-	
+
 func _physics_process(delta: float) -> void:
+	if is_on_floor():
+		is_jumping = false
 	
-	#Change Direction
-	var direction = Input.get_axis("left", "right")
+	var direction := Input.get_axis("left", "right")
 	
-	# Add the gravity
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	#Handle Speed Change
-	change_speed()
 		
-	#Handle Left Right Changes
-	if !is_dying:
-		if !is_attacking:
-			if !is_climbing:
-				if !is_sliding:
-					change_direction(direction)
+	#Handle Directions
+	facing(direction)
 	
-	#Handle Crouch
-	if !is_dying:
-		if !is_attacking:
-			if !is_climbing:
-				if !is_sliding:
-					handle_crouch()
+	#Handle Run or Walk
+	SPEED = walkorrun(SPEED)
 	
 	#Handle Jump
-	if !is_dying:
-		if !is_attacking:
-			if !is_climbing:
-				if !is_sliding:
-					if !is_crouching:
-						handle_jump()
+	jump()
 	
-	#handle the movement/deceleration.
+	# Get the input direction and handle the movement/deceleration.
 	if direction:
 		velocity.x = direction * SPEED
+		is_moving = true
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * 4 * delta)
+		velocity.x = move_toward(velocity.x, 0, friction * delta * 4)
+		is_moving = false
 		
-	play_animations(direction)
-	
+	#Handle Anims
+	play_run_walk_idle_jump_anims()
+		
 	if !is_crouching:
 		move_and_slide()
+
+func play_run_walk_idle_jump_anims():
+	if !is_crouching:
+		if is_on_floor():
+			if SPEED == runspeed and is_moving == true:
+				animator.play("run")
+			elif SPEED == walkspeed and is_moving == true:
+				animator.play("walk")
+			else:
+				animator.play("idle")
+		else:
+			if is_jumping:
+				animator.play("jump")
